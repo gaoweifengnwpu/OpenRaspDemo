ec0e835ffbe35f948293643c531f5bbdef5c29d4
gaoweifeng <gao_weifeng2@uxsino.com>
2021-05-24 11:02:12 +0800

    commit

--- src/main/java/FirstAgent.java
+++ src/main/java/FirstAgent.java
@@ -1,6 +1,7 @@
 import java.lang.instrument.Instrumentation;
 
 public class FirstAgent {
+    //    agentArgs代表传递过来的参数，inst则是agent技术主要使用的API，我们可以使用它来改变和重新定义类的行为
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("FirstAgent is Start.");
         inst.addTransformer(new FirstTransformer());
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,7 +3,7 @@ import com.alibaba.fastjson.JSON;
 public class Test {
     public static void main(String[] args) {
         User user = new User("zane");
-        System.out.println(JSON.toJSON(user));
+//        System.out.println(JSON.toJSON(user));
         System.out.println(user.toString());
     }
 }
